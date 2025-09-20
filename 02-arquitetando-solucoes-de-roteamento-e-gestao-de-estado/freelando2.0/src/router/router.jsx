33faea7c6be11972a8e4dc16f95339f6c238c116
Alberto Lima <alberto.btlima@outlook.com>
2025-09-20 15:23:55 -0300

    up

--- 02-arquitetando-solucoes-de-roteamento-e-gestao-de-estado/freelando2.0/src/router/router.jsx
+++ 02-arquitetando-solucoes-de-roteamento-e-gestao-de-estado/freelando2.0/src/router/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom"
-import PaginaInicial from "../paginas/PaginaInicial"
+import { createBrowserRouter } from "react-router-dom";
+import PaginaInicial from "../paginas/PaginaInicial";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <PaginaInicial />,
-        children: [],
-    },
+  {
+    path: "/",
+    element: <PaginaInicial />,
+    children: [],
+  },
 ]);
