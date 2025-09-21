e1290325c03c46c4008cd6ab2a279988fedd4068
Alberto Lima <alberto.btlima@outlook.com>
2025-09-21 17:18:55 -0300

    add: página conclusao

--- 02-arquitetando-solucoes-de-roteamento-e-gestao-de-estado/freelando2.0/src/paginas/cadastro/Concluido.jsx
+++ 02-arquitetando-solucoes-de-roteamento-e-gestao-de-estado/freelando2.0/src/paginas/cadastro/Concluido.jsx
@@ -1,17 +1,37 @@
+import { Botao } from "../../componentes/Botao/Botao";
+import { Link } from "react-router-dom";
 import { Tipografia } from "../../componentes/Tipografia/Tipografia";
 
 import clienteConcluido from "./assets/cliente-concluido.png";
+import styled from "@emotion/styled";
+
+const ImagemConclusao = styled.img`
+  max-width: 100%;
+  border-radius: 16px;
+`;
 
 const Concluido = () => {
   return (
     <>
-      <Tipografia variante="h1" componente="h1">
-        Seu perfil está completo!
-      </Tipografia>
-      <Tipografia variante="body" componente="body">
-        Crie seu perfil gratuitamente para começar a trabalhar com os melhores
-        freelancers.
-      </Tipografia>
+      <div style={{ textAlign: "center" }}>
+        <Tipografia variante="h1" componente="h1">
+          Seu perfil está completo!
+        </Tipografia>
+        <Tipografia variante="body" componente="body">
+          Agora é só começar a se conectar com os melhores freelancers do
+          mercado!
+        </Tipografia>
+      </div>
+
+      <figure>
+        <ImagemConclusao src={clienteConcluido} alt="Imagem conclusão" />
+      </figure>
+
+      <div style={{ textAlign: "center" }}>
+        <Link to="/cadastro">
+          <Botao variante="secundaria">Voltar para a home</Botao>
+        </Link>
+      </div>
     </>
   );
 };
